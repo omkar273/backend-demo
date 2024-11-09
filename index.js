1e9dc2216d1c713100737556344a954df0fab85a
omkar273 <omkarsonawane622@gmail.com>
2024-11-09 21:24:01 +0530

    creating utils

--- index.js
+++ index.js
@@ -1,19 +1,36 @@
 import express from "express";
-import db_connect from './../src/db/index.js';
+import db_connect from './src/db/index.js';
 import dotenv from 'dotenv';
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+
 
 dotenv.config({
     path: "./env"
 });
 
-const app = express();
+export const app = express();
+app.use(express.json({ limit: '16kb' }));
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(cors(
+    {
+        origin: process.env.CORS_ORIGIN,
+        credentials: true,
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    }
+));
+app.use(express.static('public'));
+
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
 const init = async () => {
     try {
-        db_connect();
+        await db_connect();
         app.on('error', (error) => {
             console.log(error);
         })
---
+++ src/utils/api_success.js
@@ -0,0 +1,11 @@
+class ApiResponse {
+
+    constructor(data, message, status = 200) {
+        this.status = status;
+        this.data = data;
+        this.message = message;
+        this.success = true;
+    }
+
+
+}
